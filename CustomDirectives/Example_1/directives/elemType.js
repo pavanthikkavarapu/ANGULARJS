34fcabf4ecd0c286a82ff1c247733cf8e1a8cba1
pavanthikkavarapu <pavan.ani143@gmail.com>
2017-10-26 13:45:09 +0530

    this is first commit

---
+++ CustomDirectives/Example_1/directives/elemType.js
@@ -0,0 +1,8 @@
+app.directive("elemType",elemType);
+function elemType() {
+    return{
+
+        restrict:'E',
+        template:"<div class='jumbotron'><b style='color:goldenrod'>Element Type Custom Directive</b></div>"
+    }
+}
